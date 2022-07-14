d15d61ed9a7b345f03b5e1f1873d684aab794b6b
slot dana <105584433+HOALND@users.noreply.github.com>
2022-07-15 04:09:48 +0700

    Initial commit

---
+++ spec/lagilagibosku-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LagilagiboskuView from '../lib/lagilagibosku-view';
+
+describe('LagilagiboskuView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
